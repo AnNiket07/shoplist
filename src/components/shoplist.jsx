c2c1250f89a42cbfd7d1b31a1ed5f5ccdbc5a741
Aniket Sharma <aniketsharma2147@gmail.com>
2021-07-05 20:17:07 +0530

    first commit

---
+++ src/components/shoplist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shoplist = () => {
+    return (
+        <div className="list">
+            Shoplist comes here
+        </div>
+    )
+}
+
+export default Shoplist;
